a33ff4f1ec3843d7f09965a2e249e52ce6593e01
Jasper <jjcroome@gmail.com>
2020-01-11 22:26:41 -0800

    color scales!

--- src/components/Bubble.js
+++ src/components/Bubble.js
@@ -2,9 +2,13 @@ import React, { useEffect } from "react";
 import useGlobal from "../store";
 import { Group } from "@vx/group";
 import { Circle } from "@vx/shape";
-import { scaleLinear, scaleOrdinal, scaleQuantize } from "@vx/scale";
+import { scaleLinear, scaleOrdinal } from "@vx/scale";
+import {Pack} from "@vx/hierarchy"
+import { interpolateRainbow } from "d3-scale-chromatic";
+import {scaleSequential} from "d3-scale"
+import { exoplanets } from '@vx/mock-data';
 // import { withTooltip, Tooltip } from "@vx/tooltip";
-
+console.log(exoplanets)
 const Bubble = (store, props) => {
   const width = window.innerWidth * 0.98;
   const height = window.innerHeight * 0.98;
@@ -22,28 +26,29 @@ const Bubble = (store, props) => {
     clamp: true
   });
 
-  // var lookup = {};
-  // var artistArray = [];
+  var lookup = {};
+  var artistArray = [];
 
-  // for (var item, i = 0; (item = globalState.trackData[i++]); ) {
-  //   var name = item["artist"][0]["name"];
+  for (var item, i = 0; (item = globalState.trackData[i++]);) {
+    var name = item["artists"][0]["name"];
 
-  //   if (!(name in lookup)) {
-  //     lookup[name] = 1;
-  //     artistArray.push(name);
-  //   }
-  // }
-  // var artistIndexArray = [];
-  // // map the artist index to a matching array, for the range in the color scale.
-  // for (i = 0; i < artistArray.length; i++) {
-  //   artistIndexArray.push(i / artistArray.length);
-  // }
+    if (!(name in lookup)) {
+      lookup[name] = 1;
+      artistArray.push(name);
+    }
+  }
+  var artistIndexArray = [];
+  // map the artist index to a matching array, for the range in the color scale.
+  for (i = 0; i < artistArray.length; i++) {
+    artistIndexArray.push(i / artistArray.length);
+  }
+  const color = scaleSequential(interpolateRainbow)
 
-  // const artistScale = scaleOrdinal({
-  //   domain: artistArray,
-  //   range: artistIndexArray,
-  //   clamp: true
-  // });
+  const artistScale = scaleOrdinal({
+    domain: artistArray,
+    range: artistIndexArray,
+    clamp: true
+  });
 
   // let tooltipTimeout;
 
@@ -69,23 +74,27 @@ const Bubble = (store, props) => {
     <div>
       <svg width={width} height={height}>
         <rect width={width} height={height} />
+        {/* <Pack root={bubbles} size={[width,height]}> */}
         <Group>
           {bubbles.map((track, i) => {
             const cx = xScale(track.valence);
             const cy = yScale(track.danceability);
-            const r = 14;
+            const fill = color(artistScale(track['artists'][0]['name']))
+            // const r = 14;
             return (
               <Circle
-                key={`track-${track["name"]}`}
-                className="dot"
-                cx={cx}
-                cy={cy}
-                r={r}
-                fill="#f6c431"
+              key={`track-${track["name"]}`}
+              className="dot"
+              cx={cx}
+              cy={cy}
+              r=".5vw"
+              stroke="white"
+              fill={fill}
               />
-            );
-          })}
+              );
+            })}
         </Group>
+        {/* </Pack> */}
       </svg>
     </div>
   );
