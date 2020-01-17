b1ed91667d60545bbae52836818104871c9f6c00
Jasparr77 <jjcroome@gmail.com>
2020-01-17 05:40:05 -0800

    bubble progress

--- src/components/Bubble2.js
+++ src/components/Bubble2.js
@@ -1,21 +1,20 @@
 import React, { useEffect, useRef, Component } from "react";
 import PropTypes from "prop-types";
-import useGlobal from "../store";
-import { spotifyToken, spotifyTracks } from "../actions";
+import { spotifyToken, spotifyTracks, forceCollide } from "../actions";
 // import rainbowScale from "./rainbowScale";
 import { interpolateRainbow } from "d3-scale-chromatic";
 import { scaleSequential, scaleOrdinal } from "d3-scale";
 // import { select } from "d3-selection";
 import { Group } from "@vx/group";
 import { Circle } from "@vx/shape";
-import { forceCollide } from "d3-force";
 
 class Bubble extends Component {
   state = {
     width: window.innerWidth * 0.75,
     height: window.innerHeight * 0.9,
     token: "",
-    tracks: {}
+    tracks: {},
+    d3Data: {}
   };
 
   componentWillMount() {
@@ -25,24 +24,29 @@ class Bubble extends Component {
   }
   componentDidUpdate() {
     const token = this.state.token;
-    spotifyTracks.getTracks(token).then(tracks => {
-      this.setState({ tracks: tracks });
-      forceCollide(tracks);
-    });
+    spotifyTracks
+      .getTracks(token)
+      .then(tracks => {
+        this.setState({ tracks: tracks });
+      })
+      .then(forceCollide.simulation(this.state.tracks))
+      .then(data => {
+        this.setState({ d3Data: data });
+      });
   }
 
   render() {
-    const width = "";
-    const height = "";
-    const d3Data = [];
-    const color = "";
-    const artistScale = "";
+    const width = window.innerWidth * 0.75;
+    const height = window.innerHeight * 0.9;
+    const d3Data = this.state.d3Data;
+    const color = scaleSequential(interpolateRainbow);
+    const artistScale = [];
 
     return (
       <div>
         <svg width={width} height={height}>
           <Group>
-            {d3Data.map((track, i) => {
+            {/* {d3Data.map((track, i) => {
               const cx = track["x"];
               const cy = track["y"];
               const r = (0.5 / 100) * width; // equivalent of .5vw
@@ -57,7 +61,7 @@ class Bubble extends Component {
                   fill={fill}
                 />
               );
-            })}
+            })} */}
           </Group>
         </svg>
       </div>
