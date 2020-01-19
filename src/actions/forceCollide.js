04bc1321492f4005ab4fe0055f7cb6d16dd543cb
Jasper <jjcroome@gmail.com>
2020-01-18 22:02:58 -0800

    hooray!

--- src/actions/forceCollide.js
+++ src/actions/forceCollide.js
@@ -2,30 +2,25 @@ import { forceSimulation, forceCollide, forceY, forceX } from "d3-force";
 import { scaleLinear } from "d3-scale";
 
 export const simulation = async tracks => {
-  // console.log(tracks);
   const width = window.innerWidth * 0.75;
   const height = window.innerHeight * 0.9;
   const items = tracks;
 
-  const xScale = scaleLinear()
-    .domain([0, 1])
-    .range([0, width * 3]);
-  const yScale = scaleLinear()
-    .domain([1, 0])
-    .range([height, 0]);
+  const xScale = scaleLinear([0, 1],[0, width])
+  const yScale = scaleLinear([0, 1],[height, 0])
 
   const move = forceSimulation()
     .force(
       "x",
       forceX(d => {
         return xScale(d["valence"]);
-      }).strength(0.05)
+      }).strength(0.5)
     )
     .force(
       "y",
       forceY(d => {
         return yScale(0.5);
-      }).strength(0.05)
+      }).strength(0.5)
     )
     .force(
       "collide",
--- src/components/Bubble2.js
+++ src/components/Bubble2.js
@@ -1,67 +1,128 @@
-import React, { useEffect, useRef, Component } from "react";
-import PropTypes from "prop-types";
-import { spotifyToken, spotifyTracks, forceCollide } from "../actions";
+import React, { Component } from "react";
+import { spotifyToken, spotifyTracks} from "../actions";
 // import rainbowScale from "./rainbowScale";
 import { interpolateRainbow } from "d3-scale-chromatic";
-import { scaleSequential, scaleOrdinal } from "d3-scale";
+import { scaleSequential, scaleOrdinal, scaleLinear } from "d3-scale";
+import { forceSimulation, forceCollide, forceY, forceX } from "d3-force";
+
 // import { select } from "d3-selection";
 import { Group } from "@vx/group";
 import { Circle } from "@vx/shape";
 
 class Bubble extends Component {
-  state = {
-    width: window.innerWidth * 0.75,
-    height: window.innerHeight * 0.9,
-    token: "",
-    tracks: {},
-    d3Data: {}
-  };
+
   constructor(props) {
     super(props);
+    const token = window.location.hash.split("=", 2)[1].split("&", 1)[0]
+    const width = window.innerWidth * 0.75
+    const height = window.innerHeight * 0.9
+
     this.state = {
-      token: window.location.hash.split("=", 2)[1].split("&", 1)[0]
+      d3Status: 'Not Started',
+      width: width,
+      height: height,
+      token: token,
+      tracks: {}
     };
   }
-
+  
   componentDidMount() {
-    spotifyTracks.getTracks(this.state.token).then(tracks => {
-      this.setState({ tracks: tracks });
-    });
+
+    const getTracks = token => {
+      spotifyTracks.getTracks(this.state.token).then(tracks => {
+        this.setState({ tracks: tracks })
+        this.setState({d3Status: 'pending'})
+      })
+    }
+  getTracks()
   }
+
   componentDidUpdate() {
-    forceCollide.simulation(this.state.tracks).then(data => {
-      // this.setState({ d3Data: data });
-      console.log(data);
-    });
+    if (this.state.d3Status === 'pending'){
+    const data = this.state.tracks
+    const xScale = scaleLinear([0, 1],[0, this.state.width])
+    const yScale = scaleLinear([0, 1],[this.state.height, 0])
+    const move = forceSimulation()
+    .force(
+      "x",
+      forceX(d => {
+        return xScale(d["valence"]);
+      }).strength(0.1)
+      )
+    .force(
+        "y",
+        forceY(d => {
+          return yScale(0.5);
+        }).strength(0.1)
+        )
+    .force(
+          "collide",
+          forceCollide(d => {
+            return 10;
+          }).iterations(5)
+          );
+    move.nodes(data)
+    .on("tick",(tracks)=>{
+            this.setState({d3Data: tracks})
+          })
+    this.setState({d3Status:'Completed'})
+        }
   }
+
 
   render() {
     const width = window.innerWidth * 0.75;
     const height = window.innerHeight * 0.9;
-    const d3Data = this.state.d3Data;
+
+    const d3Data = Array.from(this.state.tracks);
+
+    //Color Scale
+    var lookup = {};
+    var artistArray = [];
+
+    for (var item, i = 0; (item = d3Data[i++]); ) {
+      var name = item["artists"][0]["name"];
+
+      if (!(name in lookup)) {
+        lookup[name] = 1;
+        artistArray.push(name);
+      }
+    }
+    var artistIndexArray = [];
+    // map the artist index to a matching array, for the range in the color scale.
+    for (i = 0; i < artistArray.length; i++) {
+      artistIndexArray.push(i / artistArray.length);
+    }
     const color = scaleSequential(interpolateRainbow);
-    const artistScale = [];
+
+    const artistScale = scaleOrdinal()
+      .domain(artistArray)
+      .range(artistIndexArray);
+    //
+
 
     return (
       <div>
         <svg width={width} height={height}>
           <Group>
-            {/* {d3Data.map((track, i) => {
-              const cx = track["x"];
-              const cy = track["y"];
-              const r = (0.5 / 100) * width; // equivalent of .5vw
-              const fill = color(artistScale(track["artists"][0]["name"]));
-              return (
-                <Circle
-                  key={`point-${track["id"]}-${track["name"]}`}
-                  className="dot"
-                  cx={cx}
-                  cy={cy}
-                  r={r}
+            if (this.state.d3Status !== 'Completed')
+            {
+              d3Data.map((track, i) => {
+                const cx = track["x"];
+                const cy = track["y"];
+                const r = (1 / 100) * width; // equivalent of .5vw
+                const fill = color(artistScale(track["artists"][0]["name"]));
+                return (
+                  <Circle
+                    key={`point-${track["id"]}-${track["name"]}`}
+                    className="dot"
+                    cx={cx}
+                    cy={cy}
+                    r={r}
                   fill={fill}
-                />
-              );
-            })} */}
+                  />
+                );
+              })}
           </Group>
         </svg>
       </div>
