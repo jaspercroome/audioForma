e0f2870c9c2924edecde7b029a51f5e52f1d2353
Jasparr77 <jjcroome@gmail.com>
2020-01-22 05:25:08 -0800

    env fix

--- src/components/oAuth.js
+++ src/components/oAuth.js
@@ -3,7 +3,7 @@ import React from "react";
 export const OAuth = props => {
   const scopes = "user-library-read";
   const redirect_uri = "http://localhost:3000/callback/";
-  const spotifyKey = process.env.REACT_APP_spotifyKey || process.env.spotifykey;
+  const spotifyKey = process.env.REACT_APP_spotifyKey;
   const spotifyAuthUrl = `https://accounts.spotify.com/authorize/?client_id=${spotifyKey}&response_type=token&redirect_uri=${encodeURI(
     redirect_uri
   )}&scope=${scopes}`;
